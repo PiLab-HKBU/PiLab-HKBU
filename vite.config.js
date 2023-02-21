86d0a41f19b7ac31fe331bdfdb864cd8b2c6cc08
Yizhe ZHANG <elecoxy@gmail.com>
2023-02-21 11:02:54 +0800

    Fix bugs

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        isCustomElement: tag => tag.startsWith('ion-')
+      }
+    }
+  })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -13,5 +19,5 @@ export default defineConfig({
   },
   base: process.env.NODE_ENV === 'production'
       ? '/'
-      : '/'
+      : '/',
 })
